eac38beff146ed0fc95e2b6e2da85320e87658c6
mapokko <mapokko@hotmail.it>
2021-03-03 23:16:56 +0100

    modifiche su initPcbs e freePcb in modo che funzionino; aggiunta di funzioni ausiliarie e inizio di allocPcb

--- pcb.c
+++ pcb.c
@@ -6,8 +6,8 @@
 
 static pcb_t pcbFree_table[MAXPROC];
 static pcb_t *pcbFree_h;
-static unsigned int campo = sizeof(pcb_PTR);
-static unsigned int processo = sizeof(pcb_t)/4;
+static uint campo = sizeof(pcb_PTR);
+static uint processo = sizeof(pcb_t);
 
 /*void initPcbs(){
 	static memaddr *puntatore, *next, *prev;
@@ -64,19 +64,24 @@ static unsigned int processo = sizeof(pcb_t)/4;
 //TUTTE LE VARIABILI SONO STATIC PER MOTIVI DI DEBUG, PER FAVORE DICHIARATE LE VARIABILI COME STATIC!!
 //al termine del progetto li togliamo tutti
 
+//devo ancora commentare questa funzione
 void initPcbs(){
 	initHead();
 	
-	static memaddr *contatore;
+	static memaddr *contatore, *dimpProcesso;
+	dimpProcesso = processo;
 
 	contatore = pcbFree_table;
-	contatore  = contatore + processo;
+	contatore  = (uint) contatore + processo;
 
 	static pcb_t *pcbPointer;
+	for(int i = 1; i < MAXPROC; i++){
+		pcbPointer = contatore;
+		freePcb(pcbPointer);
 
-	pcbPointer = contatore;
-
-	freePcb(pcbPointer);
+		contatore  = (uint) contatore + processo; 
+	}
+	
 
 }
 
@@ -95,8 +100,8 @@ void initHead(){
 	//setto i puntatori next e prev con l'indirizzo di memoria dei campi p_next e p_prev
 	//così che quando li dereferenzio, poso cambiare il valore nella cella di memoria
 	//puntata da loro
-	next = pointToFirst;
-	prev = pointToFirst + campo;
+	next = (uint)pointToFirst;
+	prev = (uint)pointToFirst + campo;
 
 	//infine qua avviene la dereferenziazzione, assegnano i puntatoti p_next e p_prev del primo pcb
 	//in modo che puntino al pcb stesso
@@ -107,21 +112,56 @@ void initHead(){
 }
 
 void freePcb(pcb_t *p){
+	//variabili necessarie per gestire l'aggiunta del pcb puntato da *p
 	static memaddr *newPcb, *next, *prev, *tailAddress;
 	static pcb_t *tail;
 
+	//al termine di queste due operazioni, tail conterrà l'indirizzo di memoria dove comincia la coda
 	tailAddress = pcbFree_h;
 	tail = *tailAddress;
 
+	//queste operazioni settano next e prev con gli indirizzi dei campi p_next e p_prev
+	//del pcb da inserire nella lista
 	newPcb = p;
-	next = newPcb;
-	prev = newPcb + campo;
+	next = (uint)newPcb;
+	prev = (uint)newPcb + campo;
 
+	//vengono settati i puntotaori dei campi del pcb da reinserire
 	*next = tail;
 	*prev = pcbFree_h;
 
-	*newPcb = p;
-	tailAddress = tail + campo;
-	*tailAddress = p;
+	//viene settato il p_next della vecchia testa della lisat in modo che
+	//punti al pcb da inserire
+	next = pcbFree_h;
+	*next = p;
 
+	//analogo del campo p_prev della coda
+	prev = (uint)tail + campo;
+	*prev = p;
+
+	//imposto il pcb inserito come la nuova coda
+	pcbFree_h = p;
+}
+
+//WORK IN PROGRESS
+pcb_t *allocPcb(){
+	if (isEmpty()){
+		return NULL;
+	}
+	else{
+		pcb_t *toReturn;
+		toReturn = pcbFree_h;
+
+
+	}
+}
+
+//WORK IN PROGRESS
+int isEmpty(){
+	static memaddr *next;
+	static pcb_t *compare;
+	next = pcbFree_h;
+	compare = *next;
+
+	return !(compare == pcbFree_h);
 }
