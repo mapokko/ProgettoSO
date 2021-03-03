e1376ab79e536a6e06925ed79c12f56198080c7e
mapokko <mapokko@hotmail.it>
2021-03-03 18:39:46 +0100

    Inserimento di tutti i file nella repo su Github

---
+++ pcb.c
@@ -0,0 +1,60 @@
+#include "pcb.h"
+#include <pandos_types.h>
+#include <pandos_const.h>
+
+#define MAXPROC 20
+
+static pcb_t pcbFree_table[MAXPROC];
+static pcb_t *pcbFree_h;
+static unsigned int campo = sizeof(pcb_PTR);
+static unsigned int processo = sizeof(pcb_t);
+
+void initPcbs(){
+	static memaddr *puntatore, *next, *prev;
+
+	puntatore = pcbFree_table;
+	pcbFree_h = puntatore;
+	next = puntatore + campo;
+	prev = next + campo;
+	*next = *prev = pcbFree_h;
+
+	static pcb_t *tmp;
+
+	for(int i = 1; i < MAXPROC; i++){
+		puntatore = puntatore + processo;
+		tmp = puntatore;
+		freePcb(tmp);
+	}
+
+	return;
+}
+
+void freePcb(pcb_t *new){
+	static pcb_t *control;
+
+	static memaddr *p ,*head;
+	head = pcbFree_h;
+	p = new;
+
+	static memaddr *next2, *prev2, *headnext;
+	headnext = head + campo;
+	control = *headnext;
+
+	next2 = p + campo;
+	prev2 = next2 + campo;
+
+	*next2 = control;
+
+	*prev2 = pcbFree_h;
+
+	p = control;
+	prev2 = p + (campo * 2);
+	*prev2 = new;
+
+	p = pcbFree_h;
+	next2 = p + campo;
+	*next2 = new;
+
+	pcbFree_h = new;
+}
+
---
+++ pcb.h
@@ -0,0 +1,7 @@
+#include <pandos_types.h>
+#ifndef PCB_H_INCLUDED
+#define PCB_H_INCLUDED
+
+void initPcbs();
+//void freePcb(pcb_t *p);
+#endif
