bebcdd1bb4ad3596df911ad8683540d7110b7c72
mapokko <mapokko@hotmail.it>
2021-03-05 22:52:13 +0100

    teminato tutte le funzioni sui primi due moduli rigyardo i pcb, funzionano ma sono un po' da ottimizzare. presto lo faccio

--- pcb.c
+++ pcb.c
@@ -13,19 +13,23 @@ static uint processo = sizeof(pcb_t);
 //TUTTE LE VARIABILI SONO STATIC PER MOTIVI DI DEBUG, PER FAVORE DICHIARATE LE VARIABILI COME STATIC!!
 //al termine del progetto li togliamo tutti
 
-//devo ancora commentare questa funzione
+void setPointers(memaddr *process,pcb_t *setNext, pcb_t *setPrev){
+	pcb_t *processPtr;
+	processPtr = process;
+
+	processPtr->p_next = setNext;
+	processPtr->p_prev = setPrev;
+}
+
 void initPcbs(){
-	//chiamo la funzione initHead, che inizializza il puntatore alla testa 
-	//e imposta i cmapi p_next e p_prev del primo elemento dell'array
-	initHead();
-	
 	//variabile che serve per sapere via via gli indirizzo di mem
 	//dove cominciano i pcb
 	static memaddr *contatore;
 
-	//inizializzo contatore in modo che punti al secondo pcb dell'array
+	//inizializzo contatore in modo che punti al primo pcb dell'array
 	contatore = pcbFree_table;
-	contatore  = (uint) contatore + processo;
+
+	initHead(contatore);
 
 	//variabile che serve per puntare via via tutti i pcb dell'array
 	static pcb_t *pcbPointer;
@@ -33,78 +37,72 @@ void initPcbs(){
 	//effettuo un ciclo che fa uno scan di tutti i pcb dentro il vettore e 
 	//li inserisce nella lista alla cui testa punta pcbFree_h
 	for(int i = 1; i < MAXPROC; i++){
+		contatore  = (uint) contatore + processo; 
 		pcbPointer = contatore;
 		freePcb(pcbPointer);
-
-		contatore  = (uint) contatore + processo; 
 	}
 	
 
 }
 
-//inizializza il puntatore alla testa della lista circolare e setta anche 
-//i campi next e prev del primo elemento del array pcbFree_table in modo che puntino al pcb stesso
-void initHead(){
-	//variabili di indirizzi di memoria
-	static memaddr *pointToFirst, *next, *prev;
+void initHead(memaddr *pointer){
 
-	//inizializzo pointToFirst con l'indirizo di memoria dove comincia il primo pcb dell'array
-	pointToFirst = pcbFree_table;
-
-	//inizializzo il puntatore del alla tesat della lista, dandogli appunto l'indirizzo della testa della lista
-	pcbFree_h = pointToFirst;
-
-	//setto i puntatori next e prev con l'indirizzo di memoria dei campi p_next e p_prev
-	//così che quando li dereferenzio, poso cambiare il valore nella cella di memoria
-	//puntata da loro
-	next = (uint)pointToFirst;
-	prev = (uint)pointToFirst + campo;
-
-	//infine qua avviene la dereferenziazzione, assegnano i puntatoti p_next e p_prev del primo pcb
-	//in modo che puntino al pcb stesso
-	*next = *prev = pcbFree_h;
-
-	//N.B.: ho visto che quando si dereferenziano i dei puntatori interni della struttura, la right-side-value
-	//deve sempre essere di type pcb_t *
+	pcbFree_h = pointer;
+	setPointers(pcbFree_h, pcbFree_h, pcbFree_h);
 }
 
-//il pcb puntato fa p viene reinserito nella lista dei pcb liberi alla cui testa
-//punta pcbFree_h
 void freePcb(pcb_t *p){
-	//variabili necessarie per gestire l'aggiunta del pcb puntato da *p
-	static memaddr *newPcb, *next, *prev, *tailAddress;
-	static pcb_t *tail;
-
-	//al termine di queste due operazioni, tail conterrà l'indirizzo di memoria dove comincia la coda
-	tailAddress = pcbFree_h;
-	tail = *tailAddress;
-
-	//queste operazioni settano next e prev con gli indirizzi dei campi p_next e p_prev
-	//del pcb da inserire nella lista
+	static memaddr *newPcb;
 	newPcb = p;
-	next = (uint)newPcb;
-	prev = (uint)newPcb + campo;
 
-	//vengono settati i puntotaori dei campi del pcb da reinserire
-	*next = tail;
-	*prev = pcbFree_h;
+	if (pcbFree_h == NULL){
+		initHead(newPcb);
+		return;
+	}
 
-	//viene settato il p_next della vecchia testa della lisat in modo che
-	//punti al pcb da inserire
-	next = pcbFree_h;
-	*next = p;
-
-	//analogo del campo p_prev della coda
-	prev = (uint)tail + campo;
-	*prev = p;
-
-	//imposto il pcb inserito come la nuova coda
+	setPointers(newPcb, pcbFree_h->p_next, pcbFree_h);
+	setPointers(pcbFree_h, p, pcbFree_h->p_prev);
+	setPointers(p->p_next, p->p_next->p_next, p);
 	pcbFree_h = p;
 }
 
+pcb_t *allocPcb(){
+	if (emptyProcQ(pcbFree_h)){
+		return NULL;
+	}
+
+	static pcb_t *toReturn;
+	toReturn = pcbFree_h;
+	
+	if(pcbFree_h->p_next == pcbFree_h){
+		pcbFree_h = NULL;
+	}
+	else{
+		pcbFree_h = pcbFree_h->p_prev;
+		setPointers(pcbFree_h, pcbFree_h->p_next->p_next, pcbFree_h->p_prev);
+		setPointers(pcbFree_h->p_next, pcbFree_h->p_next->p_next, pcbFree_h);
+	}
+
+	setValues(toReturn);
+	return toReturn;
+}
+
+void setValues(pcb_t *pointer){
+	pointer->p_prev = 0;
+	pointer->p_next = 0;
+	pointer->p_prnt = 0;
+	pointer->p_child = 0;
+	pointer->p_next_sib = 0;
+	pointer->p_prev_sib = 0;
+}
+
+
+
 //WORK IN PROGRESS: teoricamente dovrebbe essere tutto qui, ma non ho ancora testato
-pcb_t *mkEmpyProcQ(){
+pcb_t *mkEmptyProcQ(){
 	pcb_t *toReturn;
+
+	toReturn = NULL;
 
 	return toReturn;
 }
@@ -120,42 +118,89 @@ int emptyProcQ(pcb_t *tp){
 }
 
 
+void insertProcQ(pcb_t **tp, pcb_t *p){
+	static memaddr *tailAddr;
+	tailAddr = tp;
 
-//WORK IN PROGRESS
-pcb_t *allocPcb(){
-	if(pcbFree_h == NULL){
-		return NULL;
+	if(emptyProcQ(*tp)){
+		initTail(tailAddr, p);
+		return;
 	}
-	else if (isOneLeft()){
-		static pcb_t *toReturn;
 
-		toReturn = pcbFree_h;
-		pcbFree_h = NULL;
-		return toReturn;
-	}
-	else{
-		pcb_t *toReturn;
-		toReturn = pcbFree_h;
+	static pcb_t *tail; 
+	tail = *tailAddr;
 
-		static memaddr *next1, *head;
+	setPointers(p, tail, tail->p_prev);
+	setPointers(tail, tail->p_next, p);
+	setPointers(p->p_prev, p, p->p_prev->p_prev);
 
-		head = toReturn;
-		next1 = (uint)head;
-
-
-
-
-
-
-	}
 }
 
-//WORK IN PROGRESS
-int isOneLeft(){
-	static memaddr *next;
-	static pcb_t *compare;
-	next = pcbFree_h;
-	compare = *next;
+void  initTail(memaddr *tailAddress, pcb_t *p){
+	*tailAddress = p;
+	setPointers(p, p, p);
+}
 
-	return !(compare == pcbFree_h);
+pcb_t *headProcQ(pcb_t **tp){
+	static memaddr *tailAddress;
+	tailAddress = tp;
+	if (*tailAddress == NULL){
+		return NULL;
+	}
+
+	static pcb_t *tail;
+	tail = *tailAddress;
+
+	return tail->p_prev;
+}
+
+pcb_t* removeProcQ(pcb_t **tp){
+	if(emptyProcQ(*tp)){
+		return NULL;
+	}
+
+	static memaddr *tailAddress;
+	static pcb_t *tailPtr;
+	tailAddress = tp;
+	tailPtr = *tailAddress;
+
+	if(tailPtr == tailPtr->p_next){
+		*tailAddress = NULL;
+	}
+	else{
+		setPointers(tailPtr->p_next, tailPtr->p_next->p_next, tailPtr->p_prev);
+		setPointers(tailPtr->p_prev, tailPtr->p_next, tailPtr->p_prev->p_prev);
+		*tp = tailPtr->p_next;
+	}
+
+	return tailPtr;
+}
+
+int isPresent(pcb_t *tail, pcb_t *p){
+	static pcb_t *scanner;
+	scanner = tail;
+	do{
+		if(scanner == p){
+			return 1;
+		}
+		scanner = scanner->p_next;
+	}while(scanner != tail);
+
+	return 0;
+}
+
+pcb_t *outProcQ(pcb_t **tp, pcb_t *p){
+	if(!isPresent(*tp, p)){
+		return NULL;
+	}
+
+	
+	setPointers(p->p_next, p->p_next->p_next, p->p_prev);
+	setPointers(p->p_prev, p->p_next, p->p_prev->p_prev);
+
+	if(p == *tp){
+		*tp = p->p_next;
+	}
+	
+	return p;
 }
