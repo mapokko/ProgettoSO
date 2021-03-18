cc00b04e1f2783f1d036bdc28209b8cceda33265
mapokko <mapokko@hotmail.it>
2021-03-18 18:00:42 +0100

    Inserimento dei file per la gestione dei pcb e dei semafori nella cartella di phse2, assicuratevi di aggiungeri nei file che create per poter usare le funzioni

---
+++ phase2/asl.c
@@ -0,0 +1,282 @@
+#include "asl.h"
+#include "pcb.h"
+#include <pandos_types.h>
+#include <pandos_const.h>
+
+
+
+
+
+/*manteniamo un valore che corrisponde alla dimensione di un semaforo, usato per inizializzare la lista dei semafori liberi*/
+static uint dimSemaforo = sizeof(semd_t);
+
+/*questa funzione inizializza tutti gli elementi della semd_table inserendoli nella lista dei semafori
+  liberi alla cui testa punta semdFree_h e alla cui coda punta semdFree_tail. Notamo che i semfaori vengono
+  inseriti nella coda e mentre vengono deallocato dalla testa*/
+extern void initASL(){
+    /*allochiamo staticamente i semafori usando un array*/
+    static semd_t semd_table[MAXPROC];
+
+    //vengono inizializzati i puntatori alla testa e alla coda della lista dei semafori liberi
+    semdFree_h = semdFree_tail = semd_table;
+
+    //viene effettuato un ciclo dove vengono inseriti tutti i semafori utilizando una funzion ausiliaria
+    for(int i = 1; i < MAXPROC; i++){
+        freeSem((uint)semdFree_h + dimSemaforo * i);
+    }
+
+    //infine si inizializzano a null l'ultimo elemnto della lista dei semafori liberi a anche i puntatori
+    //alla lista dei semafori attivi
+    semdFree_tail->s_next = semd_h = semd_tail = NULL;
+
+}
+
+/*questa funzione si occupa di inserire il pcb puntato da p nella coda dei pcb associato al semaforo a cui e' assegmato
+  il valore passato per paramtro semAdd*/
+extern int insertBlocked(int *semAdd, pcb_t *p){
+    semd_t *semPtr;
+
+    //usando una variabile intermedia e una funzione ausiliaria, viene verificato se è presente nella lista dei semafori
+    //attivi il semaforo a cui è associato il valore in semAdd. Se viene trovato, esso sarà puntato da semPtr
+    if(((semPtr = isInserted(semAdd)) != NULL)){
+        //se viene trovato, su invoca la funzione sui pcb insertProcQ, che permette si inserire in fondo alla coda associata a
+        //semPtr il pcb puntato da p
+        insertProcQ(&(semPtr->s_procQ), p);
+        //fatto ciò, si assegna il numero del semaforo al campo p_semAdd del pcb appena inserito
+        p->p_semAdd = semAdd;
+
+    }
+    //se il semaforo a cui è associato semAdd non è presente tra i semafori attivi, se ne estrae uno dai semafori liberi usando
+    //una funzione ausiliaria, gli si assegma il valore semAdd e si inizializza la sua coda dei processi inserendo il pcb puntato da p
+    else if((semPtr = allocASL(semAdd)) != NULL){
+        //si inizializza la lista dei processi associati a semPtr usando una funzione per la manipolazione delle code di pcb
+        semPtr->s_procQ = mkEmptyProcQ();
+        //si inizializza la coda usando una funzione ausiliaria dei pcb
+        initTail(&(semPtr->s_procQ), p);
+        //fatto ciò, si assegna il numero del semaforo al campo p_semAdd del pcb appena inserito
+        p->p_semAdd = semAdd;
+    }
+    
+    //infine si ritorna un booleano, che è tru se semPtr è rimastp NULL dopo l'assegnamento di entrambe le funzioni ausiliarie
+    //cioè nella lista dei semafori attivi non è presente il semaforo cercato e la lista dei semafori liberi è vuoto.
+    //in tutti gli altri casi è falso
+    return(semPtr == NULL);
+}
+
+/*questa funzione rimuove il primo pcb dalla coda dei pcb associato al semaforo identificato con il parametro passato*/
+extern pcb_t* removeBlocked(int *semAdd){
+    //usiamo una variabile ausiliaria per verificare che sia effettivamente inserito un semaforo associato al parametro passato
+    //verifichiamo inoltre che la sua coda dei processi sia vuota o meno
+    semd_t *semPtr;
+    if((semPtr = isInserted(semAdd)) == NULL || semPtr->s_procQ == NULL){
+        return NULL;
+    }
+
+    //se è presente, usiamo un'altra variabile ausiliaria per estrarre il pcb da restituire usando una funzione per la manipolazione
+    //delle code di pcb
+    pcb_t *pcbPtr;
+    pcbPtr = removeProcQ(&(semPtr->s_procQ));
+
+    //verifichiamo se, rimuovendo il pcb, abbiamo svuotato la coda dei pcbel semaforo
+    if(semPtr->s_procQ == NULL){
+        //in questo caso, usiamo una funzione ausiliaria per rimuovere il semaforo vuoto dalla lista dei semafori attivi
+        removeSem(semPtr);
+          
+    }
+
+    //infine si ritorna il pcb estratto
+    return pcbPtr;
+}
+
+/*questa funzione estrae il pcb passato per argomento dalla coda dei pcb a cui è associato, il quale è individuato usando il semaforo
+  a cui p è associato. Notiamo che p può trovarsi in qualunque posizione dentro la coda di pcb*/
+extern pcb_t* outBlocked(pcb_t *p){
+    //usiamo una variabile ausiliaria per verificare che il semaforo associato a p sia presente nella lista dei semafori attivi
+    //e che il pcb passato sia effettivamente associato ad una coda di pcb di un semaforo attivo
+    semd_t *semPtr;
+    if((semPtr = isInserted(p->p_semAdd)) == NULL || p->p_semAdd == 0){
+        return NULL;
+    }
+
+    //se le verifiche sono andate a buon fine, usiamo una variabile ausiliaria per estrarre il pcb dalla coda dei pcb attraverso
+    //una funzone di manipolazione delle code di pcb
+    pcb_t *pcbPtr;
+    pcbPtr = outProcQ(&(semPtr->s_procQ), p);
+
+    //infine verifichiamo se il il semaforo è stato svuotato da pcb associati, nel qual caso rimuoviamo il semaforo dalla lista dei
+    //semafori attivi
+    if(semPtr->s_procQ == NULL){
+        removeSem(semPtr);
+    }
+
+    //alla fine si restituisce un puntatore al pcb estratto
+    return pcbPtr;
+}
+
+/*questa funzione restituisce un puntatore alla testa della coda di pcb associato al semaforo a cui è assegnato il valore passato per
+  argomento nel suo campo s_semAdd*/
+extern pcb_t* headBlocked(int *semAdd){
+    //verifichiamo innanzitutto che il semaforo cercato è presente nella lista dei semafori attivi e che la coda dei processi associato
+    //sia vuota o meno
+    semd_t *semPtr;
+    if((semPtr = isInserted(semAdd)) == NULL || emptyProcQ(semPtr->s_procQ)){
+        return NULL;
+    }
+
+    //se le verifiche sono andate a buon fine, usiamo una variabile aausiliaria e una funzione di manipolazione delle code di pcb
+    //per estrarre un puntatore all'ultimo elemento della coda dei pcb associato al semaforo cercato
+    pcb_t *pcbPtr;
+    pcbPtr = headProcQ(&(semPtr->s_procQ));
+
+    //poiché le code di pcb sono circolarmente puntati e la funzione usata estrae il pcb in fondo alla coda, si restituisce il pcb
+    //successivo, che corrisponde a quello in testa
+    return pcbPtr->p_next;
+}
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+/*FUNZIONI AUSILIARIE PER GESTIRE I SEMAOFORI*/
+/*-------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+
+/*questa è una funzione ausiliaria che permette di inserire il semaforo puntato dall'argomento nella lista dei semafori
+  liberi, inizializzando anche i loro campi*/
+void freeSem(semd_t *sem){
+    //si verifica prima se la lista dei semafori liberi è vuota o meno
+    if(semdFree_h == NULL){
+        //se è vuota si iniziializzano i puntatori alla testa e alla cosa assieme
+        semdFree_h = semdFree_tail = sem;
+    }
+    else{
+        //altrimenti, se la lista non è vuota, si inserisce il semaforo in coda
+        semdFree_tail->s_next = sem;
+        semdFree_tail = sem;
+    }
+    //infine ne si neutralizzano i campi
+    sem->s_next = sem->s_procQ = sem->s_semAdd = NULL;
+    
+}
+
+
+
+/*funzione ausiliaria per verificare se nella lista dei semafori attivi è presente un semaforo a cui è associato il valore
+  passto per parametro semAdd. Ritorna NULL se non vien trovato o un puntatore al semaforo se viene trovato*/
+semd_t *isInserted(int *semAdd){
+    //inizializziamo una variail ausiliaria chiamata parser che serve per scandire la lista dei semafori attivi
+    semd_t *parser;
+    parser = semd_h;
+    
+    //scansionimo sequenzialmente la lista dei semafori attivi, e se viene trovato un semaforo con valore semAdd, viene subito
+    //restituito un puntatore ad esso
+    while(parser != NULL){
+        if((parser->s_semAdd) == semAdd){
+            return parser;
+        }
+        parser = parser->s_next;
+    }
+    //se l'esecuzione è arrivata qui, allora non è presente il semaforo ricercato e quindi si ritorna NULL
+    return NULL;
+}
+
+/*funzione ausiliaria per allocare un semaforo dalla lista dei semafori liberi in quella dei semafori attivi
+  Ritorna NULL se non ci sono semafori liberi o un puntatore al semaforo se ce n'e' una libera*/
+semd_t *allocASL(int *semAdd){
+    //innanzitutto verifichiamo se la lista dei semafori liberi ha effettivamente dei semafori liberi
+    if(semdFree_h == NULL){
+        //se non viene trovato un semaforo libero, ritorniamo NULL
+        return NULL;
+    }
+    else{
+        //se ha sei semafoti liberi, ne estraiamo una dalla testa e gli assegnamo il valore passato per parametro semAdd
+        //nel suo campo s_semAdd
+        semd_t *newSem;
+        newSem = semdFree_h;
+        newSem->s_semAdd = semAdd;
+
+        //corrggiamo i puntatori alla testa e alla coda della lista dei semafori liberi verificando se, estraendo questo
+        //semaofor, abbiamo completamente svuotato la lista
+        if(semdFree_h->s_next == NULL){
+            semdFree_h = semdFree_tail = NULL;
+        }
+        else{
+            semdFree_h = semdFree_h->s_next;
+        }
+
+        //usiamo una funzione ausiliaria per inserire il semaforo estratto nella posizione corretta, mantenendo un ordine
+        //ascendente tra i diversi semaforo usando come criterio il loro campo s_semAdd
+        insertSem(newSem);
+        //infine ritorniamo un puntatore al semaforo appena estratto
+        return newSem;
+    }
+
+}
+
+/*questa funzione individua la posizione corretta all'interno della lista dei semafori attivi dove inserire il semaforo passato per
+  parametro newSem, in modo che i semafori siano ordinato in modo ascendente in abse al campo s_semAdd*/
+void *insertSem(semd_t *newSem){
+    //verifichiamo innanzitutto se la lista è vuota o meno. Se è vuota viene inizializzato la lista dei de semafori attivi inserendo
+    //il semaforo passato per parametro facendo punatare sia il puntatore alla testa che quello alla coda a newSem
+    if(semd_h == NULL){
+        semd_h = semd_tail = newSem;
+        newSem->s_next = NULL;
+    }
+    else{
+        //se la lista non è vuota, vengono verificati i casi limite, cioè il caso in cui il campo s_semAdd di newSem
+        //è più piccolo di qualunque altro semaforo nella lista, o il caso in cui  è il più grande, cioè il caso in cui s_semAdd di
+        //newSem è più piccolo del semaforo in testa, quindi deve diventare la nuova testa, o se è più grande del semaforo in fondo
+        //alla coda, quindi newSem deve diventare la nuova coda
+        if(semd_h->s_semAdd > newSem->s_semAdd){
+            newSem->s_next = semd_h;
+            semd_h = newSem;
+        }
+        else if(semd_tail->s_semAdd < newSem->s_semAdd){
+            semd_tail->s_next = newSem;
+            newSem->s_next = NULL;
+            semd_tail = newSem;
+        }
+
+        //se i casi limite non sono verificati, allora dichiariamo una variabile ausiliaria per controllare sequenzialmente tutti
+        //i semafori nella lista dei semafori attivi, e fermiamo il ciclo puntando al semaforo che diventerà il precedente del nuovo semaforo
+        else{
+            semd_t *scanner;
+            scanner = semd_h;
+            while(scanner->s_next->s_semAdd < newSem->s_semAdd){
+                scanner = scanner->s_next;
+            }
+            //individuata la posizione, correggiamo i puntatori in modo da inserire il nuovo semaforo
+            newSem->s_next = scanner->s_next;
+            scanner->s_next = newSem;
+        }
+        
+    }
+    
+}
+
+
+
+/*questa funzione rimuove il semaforo passato come argomento dalla lista dei semafori attivi e lo restituisce alla lista
+  dei semafori liberi*/
+void removeSem(semd_t *semDel){
+    //verrifichiamo innanzitutto il caso limite in cui il semaforo da eliminare si trova alla testa dei semafori attivi
+    if(semDel == semd_h){
+        semd_h = semd_h->s_next;
+    }
+    else{
+        //altrimenti si usa una variabile ausiliaria per individuare la posizione del semaforo precedente a quello da eliminare
+        semd_t *scanner;
+        scanner = semd_h;
+        while(scanner->s_next != semDel){
+            scanner = scanner->s_next;
+        }
+        //si corregge il puntatore del semaforo precedente a quello da eliminare
+        scanner->s_next = semDel->s_next;
+        
+        //si verifica se il semaforo da eliminare è l'ultimo della coda, e in quel caso si corregge il puntatore alla coda della
+        //lista dei semafori attivi
+        if(semd_tail == semDel){
+            semd_tail = scanner;
+        }
+    }
+    //infine, si reinserisce il semaforo alla lista dei semafori liberi usando una funzione ausiliaria
+    freeSem(semDel);
+}
+
---
+++ phase2/pcb.c
@@ -0,0 +1,319 @@
+#include "pcb.h"
+#include <pandos_types.h>
+#include <pandos_const.h>
+
+
+
+
+/*processo è il valore usato per indicare la dimensione occupata da un pcb, ovvero la dimensione di una singola cella
+  dentro pcbFree_table. E' usato per calcolare l'offset per raggiungere i diversi inidirizzi in ci cominciano le
+  celle, ovvero i pcb di pcbFree_table*/
+static uint processo = sizeof(pcb_t);
+
+
+/*inizializza la lista dei pcb liberi aggiungendo tutti i pcb contenuti in pcbFree_table e alla testa
+  di questa lista punta pcbFree_h. Notiamo che la lista dei pcb liberi è una lista monodirezionale collegata
+  con dal campo p_next mentre il campo p_prev è settato a 0. La lista termina con l'ultimo pcb che punta a NULL*/
+extern void initPcbs(){
+	//avviene qui la dichiarazione dlell'array che assegna o spazio di memoria effettiov per tutti i successivi
+	//pcb che verranno manipolato nel corso del resto del programma
+	static pcb_t pcbFree_table[MAXPROC];
+
+	initHead(pcbFree_table);
+
+	//pcbPoitner è un puntatore a pcb che serve per scorrere via via l'array di pcbFree_table in modo da puntare
+	//i processi in pcbFree_table
+	pcb_t *pcbPointer;
+
+	for(int i = 1; i < MAXPROC; i++){
+		//in particolare, usiamo l'indirizzo di inizio da cui cominciano le celle di pcbFree_table e usiamo un offset
+		//via via crescente per identificare l'indirizzo dove cominciano le varie celle
+		pcbPointer = (uint) pcbFree_table + processo * i;
+		//viene usata la funzione freePcb per inserire i vari pcb puntati da pcbPointer all'interno della lista di pcb libero
+		//la cui testa è puntata da pcbFree_h
+		freePcb(pcbPointer);
+	}
+}
+
+/*questa fuzione prende il pcb puntato da p e lo inserisce alla testa della lista dei pcb liberi, aggiornando poi pcbFree_h*/
+extern void freePcb(pcb_t *p){
+	//viene prima di tutto verificato se la lista dei pcb è completamente vuota, in tal caso si riutilizza la funzione per 
+	//inizializzare la testa
+	if (pcbFree_h == NULL){
+		initHead(p);
+		return;
+	}
+	//viene chiamata la fuzione setPointers che si occupa di impostare i puntatori di di p, mettendo nel suo campo p_next
+	//l'indirizzo del pcb puntato al momento dal puntatore alla tesat della lista
+	setPointers(p, pcbFree_h, 0);
+	//viene poi aggiornata la testa dei pcb liberi
+	pcbFree_h = p;
+}
+
+/*qusta funzione di occupa di estrarre un pcb dalla lista dei pcb liberi e resituirla. Se la lista è vuota restituisce NULL*/
+extern pcb_t *allocPcb(){
+	//viene prima verificata se la lista è vuota o meno. Nel primo caso viene subito restituito NULL
+	if (emptyProcQ(pcbFree_h)){
+		return NULL;
+	}
+	//viene utilizata la variabile toReturn per mantere un puntatore al pcb che deve essere restituito
+	pcb_t *toReturn;
+	toReturn = pcbFree_h;
+	
+	//viene aggiornato il puntatore alla testa della lista dei pcb liberi
+	pcbFree_h = pcbFree_h->p_next;
+
+	//vengono impostato i valori del pcb da restituire
+	setValues(toReturn);
+	return toReturn;
+}
+
+/*questa funzione si occupa di tonare un valor da impostare il puntatore  ad una nuova coda di processi che è al momento vuota,
+  in particolare vediamo che il puntatore ad una coda dei processi viene inizializzata con NULL*/
+extern pcb_t *mkEmptyProcQ(){
+	return NULL;
+}
+
+/*questa funzione verifica se il puntatore a pcb tp punta ad un pcb o non punta a nulla*/
+extern int emptyProcQ(pcb_t *tp){
+	if(tp == NULL){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+
+/*questa funzione si occupa di inserire il pcb puntato da p, nella lista di processi il cui puntatore alla coda è puntato
+da tp. In particolare si occupa di inizializzare la coda dei processi nel casoil puntatore puntato da tp non punti a nulla.*/
+extern void insertProcQ(pcb_t **tp, pcb_t *p){
+	//viene verificato se il puntatore puntato da tp punta effettivamente a qualcosa o se deve essere inizializzata
+	if(emptyProcQ(*tp)){
+		//questa è una funzione che inizalizza il puntatore a una coda di processi
+		initTail(tp, p);
+		return;
+	}
+
+	//viene usata una variabile ausiliaria per manipoalare i campi del pcb puntato dal puntatore alla coda,
+	//che è a sua volta puntato da tp. Per questo motivo a tail si assegna *tp e non tp e basta
+	pcb_t *tail; 
+	tail = *tp;
+
+	//queste tre invocazioni impostano i campi p_next e p_prev rispettivamente di: il nuovo pcb puntato da p, il pcb
+	//puntato dal puntatore alla coda e infine il pcb che si trova alla testa della coda, il cui campo p_next deve essere
+	//aggiornata puntando al nuovo pcb in testa
+	setPointers(p, tail, tail->p_prev);
+	setPointers(tail, tail->p_next, p);
+	setPointers(p->p_prev, p, p->p_prev->p_prev);
+}
+
+/*questa funzione prende un puntatore al puntatore alla coda di una coda dei processi e si occupa di ritornare un puntatore
+  al pcb che si trova in testa alla cosa dei processi*/
+extern pcb_t *headProcQ(pcb_t **tp){
+	//si verifica innazitutto se il puntatore alla cosa sta effettivamente puntando alla coda
+	if (*tp == NULL){
+		return NULL;
+	}
+	//si usa una variabile intermedia per accedera i campi interni del pcb in fondo alla coda e si utilizza dil cuo campo
+	//p_prev per risalire alla testa
+	pcb_t *tail;
+	tail = *tp;
+	return tail->p_prev;
+}
+
+/*questa funzione si occupa di rimuovere il pcb che si trova in testa alla coda della coda dei processi puntata dal
+  puntatore alla coda puntato da tp*/
+extern pcb_t* removeProcQ(pcb_t **tp){
+	//semplicemente, la rimozione del pcb che si trova nella codaa da più tempo è un caso particolare di outProcQ, dove il pcb
+	//da rimuovere è quello puntatore dal puntatore alla coda
+	return outProcQ(tp, *tp);
+}
+
+/*questa funzione permette di estrarre dalla coda di pcb puntato dal puntatore contenuto in tp in modo da estarre esattamente il pcb
+  puntato da p, che si può trovare in qualunque posizione dentro la coda. In particolare, se p non è presente nella cosa contenuta in tp,
+  allora ritoriamo NULL;*/
+extern pcb_t *outProcQ(pcb_t **tp, pcb_t *p){
+	//verifichiamo se p è presente nella coda puntata da tp usanod una funzione ausiliaria
+	if(emptyProcQ(*tp) || !isPresent(*tp, p)){
+		return NULL;
+	}
+
+	//se p è contenuto nella coda passata attraverso tp, allora aggiunstiamo i puntatori dei pcb prima e dopo p
+	setPointers(p->p_next, p->p_next->p_next, p->p_prev);
+	setPointers(p->p_prev, p->p_next, p->p_prev->p_prev);
+
+	//verifichiamo se il pcb che stiamo rimovendo è in testa alla cosa, in quel caso aggiorniamo anche il puntatore alla coda. Inoltre
+	//se il pcb che sta venendo rimosso è l'unico presente, mettiamo il puntatore alla coda a NULL
+	if (p == p->p_next){
+		*tp = NULL;
+	}
+	else if(*tp == p){
+		*tp = p->p_next;
+	}
+
+	//infine prima di restituire il pcb, ne settiamo tutti i valori a un valore neutro
+	setValues(p);
+	
+	return p;
+}
+
+/*questa funzione ritorna TRUE se il pcb passato per parametro non ha figli, FALSE altrimenti*/
+extern int emptyChild(pcb_t *p){
+	return (p->p_child == NULL);
+}
+
+/*questa funzione si occupa di inserire il pcb puntato da p come ultimo figlio nella lista dei figli del pcb puntato da prnt
+  in particolare si occupa anche di inizializzare il campo p_child di prnt nel caso non abbia ancora figli*/
+extern void insertChild(pcb_t *prnt, pcb_t *p){
+	//verifichiamo se prnt ha figli o meno
+	if (emptyChild(prnt)){
+		//se non ne ha, inizializza il campo p_child di prnt e inizializza i campi di p
+		prnt->p_child = p;
+		setPointerTree(p, prnt, NULL, NULL);
+		return;
+	}
+	//si usa una variabile ausiliaria per mantenere l'ultimo figlio di prnt
+	pcb_t *lastChild;
+
+	//si usa una funzione ausiliaria che restituisce l'ultimo figlio dalla lista dei filgi di prnt
+	lastChild = getLastChild(prnt);
+
+	//si correggono i campi dell'ultimo figlio e di p in modo che diventi il nuovo ultimo figlio
+	lastChild->p_next_sib = p;
+	p->p_prev_sib = lastChild;
+	p->p_next_sib = NULL;
+
+	//si corregge il campo parent di p in modo che punti a prnt
+	p->p_prnt = prnt;
+	
+}
+
+/*questa funzione rimuove il primo figlio della lista dei figli del pcb puntato da p*/
+extern pcb_t *removeChild(pcb_t *p){
+	//verifichiamo se effettivamente p ha dei figli
+	if(emptyChild(p)){
+		p = NULL;
+	}
+	//se li ha, usiamo la funzione outchild poiché è un caso particolare in cui il pcb da rimuovere è il primo
+	//figlio della lista
+	else{
+		p = outChild(p->p_child);
+	}
+
+	return p;
+}
+
+/*questa funzione si occupa di rimuove il pcb p dalla lista dei figli a cui appartiene, in particolare p può trovarsi
+  in una qualunque posizione all'interno della lista dei figli*/
+extern pcb_t *outChild(pcb_t *p){
+	//verifichiamo se effettivamente p appartiene a una lista di figli
+	if(p->p_prnt == NULL){
+		return NULL;
+	}
+
+	//verifichiamo se p è il primo figlio della lista, e in tal caso cambiamo il primo figlio
+	if(p->p_prnt->p_child == p){
+		p->p_prnt->p_child = p->p_next_sib;
+	}
+	//se p non è il primo figlio, allora deve necessariamente avere un prev_sib, quindi correggiamo il suo campo next_sib
+	else{
+		p->p_prev_sib->p_next_sib = p->p_next_sib;
+	}
+
+	//infine verifichiamo se p ha un fratello successico, in tal caso correggiamo il suo campo prev_sib
+	if(p->p_next_sib != NULL){
+		p->p_next_sib->p_prev_sib = p->p_prev_sib;
+	}
+
+	//settiamo a valori di p a valori neutri e lo restituiamo
+	setValues(p);
+	return p;
+}
+
+
+
+/*DA QUI SOTTO COMINCIANO FUNZIONI AUSILIARE PER IL FUNZIONAMENTO DI QUELLE RICHIESTE*/
+/*------------------------------------------------------------------------------------------------------------------------*/
+/*------------------------------------------------------------------------------------------------------------------------*/
+
+
+
+/*questa funzione si occupa di impostare il puntatore pcbFree_h quando la lista dei pcb liberi è completamente vuota,
+  ovvero pcbFree_h sta puntando a NULL*/
+void initHead(memaddr *pointer){
+	pcbFree_h = pointer;
+	setPointers(pcbFree_h, NULL, 0);
+}
+
+
+/*questa funzione si occupa di impostare i campi p_next e p_rev del pcb il cui indirizzo è contenuto in ProcessPtr
+  il pcb setNext diventa quello puntato dal campo p_next e il pcb p_prev punta al pcb puntato dal campo setPrev*/
+void setPointers(pcb_t *processPtr,pcb_t *setNext, pcb_t *setPrev){
+	processPtr->p_next = setNext;
+	processPtr->p_prev = setPrev;
+}
+
+
+/*questa funzione si occupa di impostare tutti i campi dei puntatori dentro al pcb a 0. Osserviamo che imposta anche il
+  che serve poi per mantnere l'indirisso del semaforo ad esso associato*/
+void setValues(pcb_t *pointer){
+	pointer->p_prev = 0;
+	pointer->p_next = 0;
+	pointer->p_prnt = NULL;
+	pointer->p_child = NULL;
+	pointer->p_next_sib = NULL;
+	pointer->p_prev_sib = NULL;
+	pointer->p_semAdd = NULL;
+}
+
+/*questa funzione si occupa di inizializzare la coda dei processi la cui coda punta tailAddress, con il solo pcb puntato da p.
+  In particolare il pcb puntato da p viene inizializzato con con i campi p_next e p_prev che puntano a se stesso*/
+void  initTail(memaddr *tailAddress, pcb_t *p){
+	*tailAddress = p;
+	setPointers(p, p, p);
+}
+
+/*questa funzione ausiliaria prende in input un puntatore a coda i pcb e un puntatroe a un certo pcb, effettua una scansione
+  lineare della coda finché non trova p. Se non lo trova restituisce 0, se lo trova restituisce 1*/
+int isPresent(pcb_t *tail, pcb_t *p){
+	//si usa una variabile ausiliaria per confrontare ogni elemento della lista
+	pcb_t *scanner;
+	scanner = tail;
+	
+	//si confronta p con scanner finché scanner non ritorna a puntare su tail. Osserviamo che se la coda puntata da tail contiene
+	//un solo elemento, il ciclo do while viene eseguito almeno una sola volta, nel caso quel pcb fosse quello ricercato
+	do{
+		if(scanner == p){
+			return 1;
+		}
+		scanner = scanner->p_next;
+	}while(scanner != tail);
+
+	return 0;
+}
+
+
+/*questa è una funzione ausiliaria che si occupa di settare i campi parent, nextsib, prevsib del pcb passato
+  come parametro*/
+void setPointerTree(pcb_t *p, pcb_t *prnt, pcb_t *nextSib,pcb_t *prevSib){
+	p->p_prnt = prnt;
+	p->p_next_sib = nextSib;
+	p->p_prev_sib = prevSib;
+}
+
+
+/*questa è unz funzione ausiliaria che restituisce l'ultimo filgio dalla lista dei figli del pcb passato
+  come parametro*/
+pcb_t* getLastChild(pcb_t *prnt){
+	//usiamo una variabile ausiliaria per raggiungere l'ultimo figlio attraverso una visita sequenziale della lista dei figli
+	pcb_t *child;
+	//inizializziamo child come primo figlio del pcb passato per parametro
+	child = prnt->p_child;
+
+	//usiamo un ciclo while per raggiungere l'ultimo figlio e lo restituiamo
+	while(child->p_next_sib != NULL){
+		child = child->p_next_sib;
+	}
+	return child;
+}
+
