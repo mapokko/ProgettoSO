a987926f7f4976752393887862d910c70375ec13
nico <nicolodenale@gmail.com>
2021-08-11 12:27:49 +0200

    caricato write to printer sbagliato

--- phase3/sysSupport.c
+++ phase3/sysSupport.c
@@ -17,7 +17,7 @@ void generalSupHandler(){
         supportSyscallHandler(&(sPtr->sup_exceptState[GENERALEXCEPT]));
     }
     else{
-        
+
     }
 }
 
@@ -54,17 +54,58 @@ void getTOD(state_t *suppState){
     LDST(suppState); /*serve????*/
 }
 
+void Write_To_Printer () {
+
+  int stringL = currentState->reg_a2;
+
+  if(stringL>128 || stringL<1) {
+
+    SYSCALL(TERMPROCESS, 0, 0, 0);
+
+  }
+
+  support_t *Ptr = SYSCALL(GETSUPPORTPTR, 0, 0, 0);
+  int DevNo = Ptr->sup_asid;
+  dtpreg_t *devReg;
+  devReg = getDevReg(4, DevNo);
+  devReg->command = BACKWRITE;
+  int* number;
+  number = currentState->reg_a1;
+  devReg->data0 = *number;
+
+  for (int i = 0; i < stringL; i++) {
+
+    SYSCALL(IOWAIT, FLASHINT, flashDevNo, 0);
+
+    number++
+    devReg->data0 = *number;
+    devReg->command = BACKWRITE;
+
+  }
+
+  if ( devReg->status == 1) {
+
+    currentState->reg_v0 = stringL;
+
+  } else {
+
+    currentState->reg_v0 = -1;
+
+  }
+
+}
+
 void Write_To_Terminal(char *string, int len){
-    
+
     if(len < 0 || len > 128){
         terminate();
     }
-    
+
     int counter = 0;
     static termreg_t *termReg;
     termReg = getDevReg(TERMINT, sPtr->sup_asid - 1);
     int opStatus;
-    
+
     SYSCALL(PASSEREN, &(supDevSem[2][sPtr->sup_asid - 1]), 0, 0);
     FERMATIsys();
     while(*string != EOS){
