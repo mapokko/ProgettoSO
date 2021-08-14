1d45d87057078e7578d363106fb7e502a96ec02e
mapokko <mapokko@hotmail.it>
2021-08-14 16:43:31 +0200

    commenti e modifica del trap handler e come viene chiamato

--- phase3/phase3/sysSupport.c
+++ phase3/phase3/sysSupport.c
@@ -4,6 +4,8 @@
 #include "vmSupport.h"
 #include "initProc.h"
 #include <umps3/umps/libumps.h>
+
+/*  dichiarazioni delle funzioni nel file corrente*/
 void supportSyscallHandler(state_t *genState, support_t *sPtr);
 void terminate(support_t *sPtr);
 void getTOD(state_t *suppState);
@@ -12,12 +14,16 @@ void Write_To_Terminal(char *string, int len, support_t *sPtr);
 void Read_From_terminal(char *stringAddr, support_t *sPtr);
 void trapHandler(int supSem, support_t *sPtr);
 
+/*  gestore generale delle funzioni di support, smista tra SYSCALL > 8 e trap handler*/
 void generalSupHandler(){
+
+    /*  si ottiene la struttura di supporto e si estrae la causa*/
     support_t *sPtr = (support_t *) SYSCALL(GETSUPPORTPTR, 0, 0, 0);
     unsigned int genExCause;
     genExCause = (sPtr->sup_exceptState[GENERALEXCEPT].cause & GETEXECCODE) >> 2;
     
-
+    /*  se la causa è una SYSCALL, si continua con il gestore delle SYSCALL di supporto
+        altrimenti è program trap*/
     if(genExCause == 8){
         supportSyscallHandler(&(sPtr->sup_exceptState[GENERALEXCEPT]), sPtr);
     }
@@ -26,8 +32,14 @@ void generalSupHandler(){
     }
 }
 
+/*  gestore delle SYSCALL di supporto*/
 void supportSyscallHandler(state_t *genState, support_t *sPtr){
+
+    /*  si incrementa il program counter all'indirizzo successivo alla chiamata della 
+        SYSCALL per riprendere correttamente l'esecuzione con un eventuale LDST*/
     genState->pc_epc += 4;
+
+    /*  si smista tra la SYSCALL corretta, se SYSCALL > 13, si chiama trapHandler*/
     switch(genState->reg_a0){
         case TERMINATE:
         terminate(sPtr);
@@ -50,45 +62,73 @@ void supportSyscallHandler(state_t *genState, support_t *sPtr){
     }
 }
 
+/*  SYSCALL 9: wrapper per SYSCALL 2*/
 void terminate(support_t *sPtr){
+    /*  prima di terminare il processo, invalita tutti i frame della swap pool
+        impiegati dal processo corrente*/
     for(int i = 0; i < POOLSIZE; i++){
         if(sPtr->sup_asid == swapPoolTable[i].sw_asid){
             swapPoolTable[i].sw_asid = -1;
         }
     }
+
+    /*  viene fatto un SYS4 per sbloccare lo instantiatorProcess*/
     SYSCALL(VERHOGEN, (int)&(masterSem), 0, 0);
+
+    /*  si elimina il processo chiamando SYS2*/
     SYSCALL(TERMPROCESS, 0, 0, 0);
 }
 
+/*  SYSCALL 10: scrive nel registro di ritorno il TOD*/
 void getTOD(state_t *suppState){
     STCK(suppState->reg_v0);
-    LDST(suppState); /*serve????*/
+    LDST(suppState);
 }
 
+/*  SYSCALL 11: stampa nel Printer*/
 void Write_To_Printer (char *string, int len, support_t *sPtr) {
+
+    /*  si verifica che la stringa sia entro i limiti di lunghezza e l'indirizzo di
+        memoria virtuale da cui si legge il carattere rientri nell'address space del U-proc*/
     if(len > 128 || len < 1 || (int)string < 0x80000000 || (int)string > 0xC0000000 ){
         terminate(sPtr);
     }
 
+    /*  si estrae il registro del printer associato al U-proc*/
     dtpreg_t *printReg = (dtpreg_t *)getDevReg(PRNTINT, sPtr->sup_asid - 1);
-    int opStatusP = 0;
+    int opStatusP;
+
+    /*  si richiede l'accesso al semaforo di support level per il printer associato*/
     SYSCALL(PASSEREN, (int)&(supDevSem[1][sPtr->sup_asid - 1]), 0, 0);
 
+    /*  ciclo che stampa un carattere alla volta nel printer*/
     while(*string != EOS){
+
+        /*  imposta il campo DATA0 del registro del Printer con un carattere della stringa*/
         printReg->data0 = *string;
 
+        /*  si disabilitano gli interrupt per evitare che arrivi prima della SYS5
+            si da poi il comando usando il campo COMMAND del printer e si blocca il processo
+            al corretto semaforo di interrupt per il device*/
         setSTATUS(getSTATUS() & ~IECON);
         printReg->command = 2;
         opStatusP = SYSCALL(IOWAIT, PRNTINT, sPtr->sup_asid - 1, 0);
         setSTATUS(getSTATUS() | IECON);
 
+        /*  ripresa l'esecuzione, si verifica se l'operazione è andata a buon fine.
+            in caso negativo, si esce dal ciclo*/
         if(opStatusP != 1){
             break;
         }
+
+        /*  si avanza di un carattere*/
         string++;
         
     }
 
+    /*  se la trasmissione della stringa ha avuto successo fino all'ultimo
+        carattere, si comunica nel registro di ritorno la lunghezza della stringa trasmessa
+        altrimento il negato dello stato di errore*/
     if ( opStatusP == 1) {
         sPtr->sup_exceptState[GENERALEXCEPT].reg_v0 = len;
 
@@ -96,73 +136,114 @@ void Write_To_Printer (char *string, int len, support_t *sPtr) {
         sPtr->sup_exceptState[GENERALEXCEPT].reg_v0 = ~opStatusP;
     }
 
+    /*  si libera l'accesso al deviec printer*/
     SYSCALL(VERHOGEN, (int)&(supDevSem[1][sPtr->sup_asid - 1]), 0, 0);
+
+    /*  si riprende l'esecuzione del U-proc*/
     LDST(&(sPtr->sup_exceptState[GENERALEXCEPT]));
 }
 
+/*  SYSCALL 12: stampa su terminale la stringa passata*/
 void Write_To_Terminal(char *string, int len, support_t *sPtr){
 
+    /*  si verifica che la stringa sia entro i limiti di lunghezza e l'indirizzo di
+        memoria virtuale da cui si legge il carattere rientri nell'address space del U-proc*/
     if(len > 128 || len < 1 || (int)string < 0x80000000 || (int)string > 0xC0000000 ){
         terminate(sPtr);
     }
 
+    /*  si individua il registro del terminale associato al U-proc*/
     termreg_t *termReg = (termreg_t *)getDevReg(TERMINT, sPtr->sup_asid - 1);
     int opStatus;
 
+    /*  si blocca l'accesso al semaforo del terminale in scrittura*/
     SYSCALL(PASSEREN, (int)&(supDevSem[2][sPtr->sup_asid - 1]), 0, 0);
     
+    /*  ciclo che stampa un carattere alla volta nel terminale*/
     while(*string != EOS){
+
+        /*  si disabilitano gli interrupt per evitare che arrivi prima della SYS5
+            si da poi il comando usando il campo COMMAND del terminale in scrittura, 
+            impostando anche il carattere da trasmettere e si blocca il processo
+            al corretto semaforo di interrupt per il device*/
         setSTATUS(getSTATUS() & ~IECON);
         termReg->transm_command = 2 | ((*string) << 8);
         opStatus = SYSCALL(IOWAIT, TERMINT, sPtr->sup_asid - 1, 0);
         setSTATUS(getSTATUS() | IECON);
         
+        /*  se l'operazione non è andata a buon fine si esce dal ciclo*/
         if((opStatus & 0xFF) != 5){
             break;
         }
+
+        /*  si procede al char successivo*/
         string++;
     }
     
+    /*  se l'intera trasmissione ha avuto successo, si comunica
+        la lunghezza delal stringa, altrimenti il negato dello stato di errore*/
     if ( (opStatus & 0xFF) == 5) {
         sPtr->sup_exceptState[GENERALEXCEPT].reg_v0 = len;
 
     } else {
         sPtr->sup_exceptState[GENERALEXCEPT].reg_v0 = ~opStatus;
     }
+
+    /*  si rilascia l'accesso al terminale in scrittura*/
     SYSCALL(VERHOGEN, (int)&(supDevSem[2][sPtr->sup_asid - 1]), 0, 0);
   
+    /*  si riprende l'esecuzione del U-proc*/
     LDST(&(sPtr->sup_exceptState[GENERALEXCEPT]));
 }
 
+/*  SYSCALL 13: legge dal terminale*/
 void Read_From_terminal(char *stringAddr, support_t *sPtr){
+
+    /*  si verifica che l'indirizzo di memoria virtuale dove si deve scrivere la stringa 
+        rientri nell'address space del U-proc*/
     if((int)stringAddr < 0x80000000 || (int)stringAddr > 0xC0000000){
         terminate(sPtr);
     }
 
+    /*  si individua il registro del terminale associato al U-proc, si mantiene inoltre
+        un intero "count" che indica il numero di caratteri letti*/
     termreg_t *termRegRead = (termreg_t *)getDevReg(TERMINT, sPtr->sup_asid - 1);
     int opStatusRead, count = 0;
     char receivedChar;
     
+    /*  si richiede l'accesso al terminale in lettura*/
     SYSCALL(PASSEREN, (int)&(supDevSem[3][sPtr->sup_asid - 1]), 0, 0);
     
-    
+    /*  si leggono i caratteri del termianel fino ad una newline*/
     while(receivedChar != '\n'){
 
+        /*  si disabilitano gli interrupt per evitare che arrivi prima della SYS5
+            si da poi il comando usando il campo COMMAND del terminale in lettura,
+            si blocca dunque il processo al corretto semaforo di interrupt per il device*/
         setSTATUS(getSTATUS() & ~IECON);
         termRegRead->recv_command = 2;
         opStatusRead = SYSCALL(IOWAIT, TERMINT, sPtr->sup_asid - 1, 1);
         setSTATUS(getSTATUS() | IECON);
 
+        /*  si estrae il carattere letto dal campo STATUS in lettura e
+            lo si scrive nell'indirizzo di memoria richiesto*/
         receivedChar = (opStatusRead & 0xFF00) >> 8;
         *stringAddr = receivedChar;
+
+        /*  se l'operazione non è andata a buon fine, si esce dal ciclo*/
         if((opStatusRead & 0xFF) != 5){
             break;
         }
+
+        /*  si incrementa il numero di caratteri letti e si procede
+            all'indirizzo successivo*/
         count++;
         stringAddr++;
         
     }
 
+    /*  se l'intera trasmissione ha avuto successo, si scrive nel registro di ritorno
+        il numero di caratteri letti, altrimenti il negato dello stato di errore*/
     if ( (opStatusRead & 0xFF) == 5) {
         sPtr->sup_exceptState[GENERALEXCEPT].reg_v0 = count;
 
@@ -170,18 +251,33 @@ void Read_From_terminal(char *stringAddr, support_t *sPtr){
         sPtr->sup_exceptState[GENERALEXCEPT].reg_v0 = ~opStatusRead;
     }
 
-
+    /*  si libera l'accesso al terminale in lettura*/
     SYSCALL(VERHOGEN, (int)&(supDevSem[3][sPtr->sup_asid - 1]), 0, 0);
+
+    /*  si riprende l'esecuzione del U-proc*/
     LDST(&(sPtr->sup_exceptState[GENERALEXCEPT]));
 
 }
 
+/*  funzione che esegue il compito della program trap*/
 void trapHandler(int supSem, support_t *sPtr){
+
+    /*  si verifica se lo U-proc detiene il controllo su qualche semaforo di supporto
+        per i device, e in tal caso si effettua una SYS4*/
     for(int i = 0; i < 4; i++){
         if(!(supDevSem[i][supSem])){
             SYSCALL(VERHOGEN, (int)&(supDevSem[i][supSem]), 0, 0);
         }   
     }
+
+    /*  se la program trap è stata richiesta con uno specifico numero di SYSCALL (20)
+        signfica che lo U-proc da eliminare detiene in controllo del semaforo della swap
+        pool table, e in tal caso esso viene liberato*/
+    if(sPtr->sup_exceptState[GENERALEXCEPT].reg_a0 == BLOCKEDTRAP){
+        SYSCALL(VERHOGEN, (int)&(swapPoolSem), 0, 0);
+    }
+
+    /*  si chiama la funzione che definisce SYSCALL 9*/
     terminate(sPtr);
 }
 
--- phase3/phase3/vmSupport.c
+++ phase3/phase3/vmSupport.c
@@ -5,23 +5,37 @@
 #include "initProc.h"
 #include <umps3/umps/libumps.h>
 
+/*  dichiarazioni globali della variabili di supporto
+    tra cui la Swap Pool Table*/
 swap_t swapPoolTable[POOLSIZE];
+
+/*  variabile che punta all'indirizzo del primo frame della swap pool*/
 static memaddr swapPoolPtr;
+
+/*  semaforo che regola l'accesso alla swap pool*/
 int swapPoolSem;
+
+/*  variabile usata dall'algoritmo di paging per determinare
+    il frame*/
 static int frameNo;
+
 swap_t *pagingAlgo(memaddr *frame, int blockNo);
 
 /*  i semafori di supporto sono così strutturati: (SOGGETTO AD EVENTUALE COMBIAMENTO)
-    *i semafori supDevSem[0][0...7] definiscono i device flash (in ordine)
-    *i semafori supDevSem[1][0...7] definiscono i printer (in ordine) 
-    *i semafori supDevSem[2][0...7] definiscono i terminali in scrittura (in ordine)
-    *i semafori supDevSem[3][0...7] definiscono i terminali in lettura (in ordine)*/
+    i semafori supDevSem[0][0...7] definiscono i device flash (in ordine)
+    i semafori supDevSem[1][0...7] definiscono i printer (in ordine) 
+    i semafori supDevSem[2][0...7] definiscono i terminali in scrittura (in ordine)
+    i semafori supDevSem[3][0...7] definiscono i terminali in lettura (in ordine)*/
 int supDevSem[4][8];
 
+/*  funzione che calcola l'ultimo frame di memoria occupato dal sistema operativo
+    così da inizializzare la Swap Pool dal primo frame di RAM disponibile*/
 memaddr swapPoolBegin(){
     return *((memaddr *)(KERNELSTACK + 0x0018)) + *((memaddr *)(KERNELSTACK + 0x0024));
 }
 
+/*  funzione che inizializza ad 1 i semafori di supporto dei dispositivi
+    Flash, Printer e Terminal*/
 void initSupSem(){
     for(int i = 0; i < 4; i++){
         for(int j = 0; j < 8; j++){
@@ -30,11 +44,13 @@ void initSupSem(){
     }
 }
 
+/*  funzione chiamata dal instatiatorProcess, inizializza tutte le variabili di supporto*/
 void initSwapPool(){
     frameNo = -1;
     swapPoolSem = 1;
     swapPoolPtr = swapPoolBegin();
 
+    /*  inizializzazione dei valori della swap pool table come vuoti, cioè con ASID negativo*/
     for(int i = 0; i < POOLSIZE; i++){
         swapPoolTable[i].sw_asid = -1;
         swapPoolTable[i].sw_pageNo = 0;
@@ -42,11 +58,23 @@ void initSwapPool(){
     }
 }
 
+/*  funzione che contiene l'algoritmo di paging, viene calcolato l'indirizzo
+    del frame della swap pool da utilizzare come e restituito l'indice
+    di tale frame nella Swap Pool Table*/
 swap_t *pagingAlgo(memaddr *frame, int blockNo){
+
+    /*  il numero del frame non può eccedere POOLSIZE/ / 2 */
     if(frameNo == ((POOLSIZE / 2) + 1)){
         frameNo = -1;
     }
+
+    /* il numero del frame viene incrementato linearmente*/
     frameNo++;
+
+    /*  a seconda del tipo di frame del U-proc da caricare in memoria,
+        viene scelto una porzione della swap pool diversa. La metà inferiore
+        è dedicata ai U-proc frame di codice mentre la metà superiore mantiene
+        i U-proc frame di stack*/
     if(blockNo != 31){
         *frame = swapPoolPtr + (0x1000 * (frameNo % (POOLSIZE / 2)));
         return &(swapPoolTable[frameNo % (POOLSIZE / 2)]);
@@ -57,13 +85,20 @@ swap_t *pagingAlgo(memaddr *frame, int blockNo){
     }
 }
 
+/*  funzione che aggiorna il TLB*/
 void updateTLB(pteEntry_t *entry){
 
+    /*  si effettua una ricerca della Entry nel TLB*/
     setENTRYHI(entry->pte_entryHI);
     TLBP();
+
+    /*  TLBP potrebbe manipolare EntryHi, quindi viene
+        caricato nuovamente*/
     setENTRYHI(entry->pte_entryHI);
     setENTRYLO(entry->pte_entryLO);
 
+    /*  se la entry viene trovata, la si aggiorna
+        altrimenti si sceglie una entry random*/
     if(!(getINDEX() & PRESENTFLAG)){
         TLBWI();
     }
@@ -72,44 +107,69 @@ void updateTLB(pteEntry_t *entry){
     }
 }
 
-
+/*  funzione che legge dal Flash device e carica nella RAM, oppure scrive nel
+    Flash device leggendo dalla RAM, il parametro "flag" indica quale delle
+    due operazioni eseguire*/
 void RWflash(int blockNo, memaddr ramAddr, int flashDevNo, int flag){
 
-
+    /*  si blocca l'accesso dal device Flash*/
     SYSCALL(PASSEREN, (int)&(supDevSem[0][flashDevNo]), 0, 0);
 
+    /*  viene estratto il registro del Flash device associato al processo*/
     dtpreg_t *devReg;
     devReg = (dtpreg_t *) getDevReg(4, flashDevNo);
+
+    /*  viene indicato al flash device da quel di memoria fisica bisogna
+        leggere o scrivere*/
     devReg->data0 = ramAddr;
 
+    /*  si disabilitano gli interrupt per eseguire l'operazione in modo atomico*/
     setSTATUS(getSTATUS() & ~IECON);
 
+    /*  viene dato il comando al flash device*/
     devReg->command = (blockNo << 8) | flag;
 
-    
+    /*  si blocca il U-proc sul semaforo di interrupt del Flash device associato*/
     SYSCALL(IOWAIT, FLASHINT, flashDevNo, 0);
     
+    /*  una volta sbloccato, si abilitano gli interrupt*/
     setSTATUS(getSTATUS() | IECON);
 
+    /*  se l'operazione non è andata a buon fine, si attiva una program trap
+        richiedendo una syscall inesistente ma con un numero particolare che
+        indica al trap Handler che il semaforo della Swap Pool Table deve
+        essere liberato*/
     if(devReg->status != 1){
-        /*chiamare gestore dei program trap*/
+        SYSCALL(BLOCKEDTRAP, 0, 0, 0);
     }
 
+    /*  viene liberato il semaforo del Flash Device*/
     SYSCALL(VERHOGEN, (int)&(supDevSem[0][flashDevNo]), 0, 0);
 }
 
-
+/*  se avviene un page fault, l'viene chiamata la funzione passUpOrDie e si
+    esegue questa funzione*/
 void Pager(){
+
+    /*  si ottiene la struttura di supporto e si determina la causa dell'eccezione*/
     support_t *sPtr = (support_t *) SYSCALL(GETSUPPORTPTR, 0, 0, 0);
     state_t *exState = &(sPtr->sup_exceptState[PGFAULTEXCEPT]);
     unsigned int exCause;
     exCause = (exState->cause & GETEXECCODE) >> 2;
-    if(exCause == 1 ){
-        trapHandler(sPtr->sup_asid - 1);
+
+    /*  pandOS imposta tutte le entry delle page table come read-writable,
+        se un eccezzione TLB Modification viene alzato, si termina il processo
+        con un program trap*/
+    if(exCause == TLBMODEXC ){
+        SYSCALL(REGULARTRAP, 0, 0, 0);
     }
 
+    /*  viene richiesto l'accesso alla swap pool table*/
     SYSCALL(PASSEREN, (int)&(swapPoolSem), 0, 0);
 
+    /*  si estrae l'indice della page table del U-proc da EntryHi
+        e si chiama l'algoritmo di paging per individuare il frame
+        della swap pool da usare*/
     int excPageNo;
     memaddr frameAddr;
     swap_t *poolTablePtr;
@@ -122,22 +182,33 @@ void Pager(){
 
     poolTablePtr = pagingAlgo(&(frameAddr), excPageNo);
     
+    /*  se il frame non è libero, bisogna aggiornale l'eventuale entry del TLB
+        e ricopiare il contenuto del frame del flash device associato al U-proc che
+        detiene il frame appena estratto*/
     if(poolTablePtr->sw_asid != -1){
 
+        /*  si disabilitano gli interrupt per eseguire atomicamente
+            l'aggioranamento del TLB*/
         setSTATUS(getSTATUS() & ~IECON);
 
+        /*  si invalida la entry nella page table*/
         poolTablePtr->sw_pte->pte_entryLO &= ~VALIDON;
-        
+
+        /*  si cerca la entry nel TLB*/
         setENTRYHI(poolTablePtr->sw_pte->pte_entryHI);
         TLBP();
+
+        /*  se presente, la si aggiorna invalidandola*/
         if(!(getINDEX() & PRESENTFLAG)){
             setENTRYHI(poolTablePtr->sw_pte->pte_entryHI);
             setENTRYLO(poolTablePtr->sw_pte->pte_entryLO);
             TLBWI();
         }
 
+        /*  si riabilitano gli interrup*/
         setSTATUS(getSTATUS() | IECON);
         
+        /*  si estrae l'indice del blocco nel Flash device assiociato al frame della swap pool*/
         int blockToUpload, PFNtoUpload;
 
         blockToUpload = (poolTablePtr->sw_pte->pte_entryHI & GETPAGENO) >> VPNSHIFT;
@@ -145,29 +216,35 @@ void Pager(){
             blockToUpload = 31;
         }
 
+        /*  si estrae l'indirizzo fisico del frame nella swap pool*/
         PFNtoUpload = poolTablePtr->sw_pte->pte_entryLO & 0xFFFFF000;
 
+        /*  si riscrive il contenuto del frame nel flash device*/
         RWflash(blockToUpload, PFNtoUpload, poolTablePtr->sw_asid - 1, FLASHWRITE);
 
     }
     
+    /*  si copia il contenuto del flash device nel frame della swap pool*/
     RWflash(excPageNo, frameAddr, sPtr->sup_asid - 1, FLASHREAD);
 
-
+    /*  si aggiorna la entry della swap pool table*/
     poolTablePtr->sw_asid = sPtr->sup_asid;
     poolTablePtr->sw_pageNo = excPageNo;
     poolTablePtr->sw_pte = &(sPtr->sup_privatePgTbl[excPageNo]);
 
+    /*  si disabilitano gli intertuper per aggioranere atomicamente la page table
+        e il TLB*/
     setSTATUS(getSTATUS() & ~IECON);
     
     sPtr->sup_privatePgTbl[excPageNo].pte_entryLO = frameAddr | VALIDON | DIRTYON;
-    
     updateTLB(&(sPtr->sup_privatePgTbl[excPageNo]));
 
     setSTATUS(getSTATUS() | IECON);
 
+    /*  viene liberato l'accesso allo swap pool table*/
     SYSCALL(VERHOGEN, (int)&(swapPoolSem), 0, 0);
     
+    /*  si riprende l'esecuzione del processo corrente*/
     LDST(exState);
 
 }
